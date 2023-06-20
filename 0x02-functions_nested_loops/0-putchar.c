ade94178a4290ace69929f13939ac8889e88e54c
yvesmfura <132387277+yvesmfura@users.noreply.github.com>
2023-06-20 13:39:40 -0600

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+	/**
+	 * main - servers to print _putchar
+	 * no use of standard libraries
+	 * Return: 0
+	 */
+
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
