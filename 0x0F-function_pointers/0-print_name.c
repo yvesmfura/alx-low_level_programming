981217acf4a660236ba94c489c595d6033c25a67
yvesmfura <mfurayves25@gmail.com>
2023-07-19 14:33:57 -0700

    tasks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name -  prints a name
+ * @name: name of the person
+ * @f: pointer to function
+ * return nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
