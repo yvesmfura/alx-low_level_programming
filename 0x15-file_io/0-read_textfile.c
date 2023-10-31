39b499d5e33f9cff85e28b23090985beb38c6e06
yves mfura <mfurayves25@gmail.com>
2023-10-31 12:32:27 -0700

    Task on i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,47 +4,48 @@
 #include <unistd.h>
 
 /**
- * Reads a specified number of bytes from a text file and writes them to standard output.
- *
- * @param filename The name of the text file to read from.
- * @param letters The number of bytes to read from the text file.
- * @return The number of bytes read from the text file, or 0 on error.
+ * read_textfile - Reads a specified number of bytes from a text file
+ *                 and writes them to standard output.
+ * @filename: The name of the text file to read from.
+ * @letters: The number of bytes to read from the text file.
+ * Return: The number of bytes read from the text file, or 0 on error.
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
-  int file;
-  char *buffer;
-  ssize_t bytes_read;
-  ssize_t bytes_written;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	char *buffer;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
 
-  if (filename == NULL) {
-    return 0;
-  }
+	if (filename == NULL)
+		return (0);
 
-  file = open(filename, O_RDONLY);
-  if (file == -1) {
-    return 0;
-  }
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
 
-  buffer = malloc(letters);
-  if (buffer == NULL) {
-    close(file);
-    return 0;
-  }
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(file);
+		return (0);
+	}
 
-  bytes_read = read(file, buffer, letters);
-  if (bytes_read == -1) {
-    free(buffer);
-    close(file);
-    return 0;
-  }
+	bytes_read = read(file, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
 
-  bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-  free(buffer);
-  close(file);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	free(buffer);
+	close(file);
 
-  if (bytes_written != bytes_read) {
-    return 0;
-  }
+	if (bytes_written != bytes_read)
+		return (0);
 
-  return bytes_read;
+	return (bytes_read);
 }
+
