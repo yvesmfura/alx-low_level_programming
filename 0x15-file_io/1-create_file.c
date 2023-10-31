02a966f2bd90e8102a7f357c3ac0e71c48b45730
yves mfura <mfurayves25@gmail.com>
2023-10-31 13:00:05 -0700

    task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - Creates a file with specified content.
+ * @filename: The name of the file to create.
+ * @text_content: The text content to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t bytes_written = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[bytes_written] != '\0')
+			bytes_written++;
+
+		if (write(file, text_content, bytes_written) == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	close(file);
+	return (1);
+}
